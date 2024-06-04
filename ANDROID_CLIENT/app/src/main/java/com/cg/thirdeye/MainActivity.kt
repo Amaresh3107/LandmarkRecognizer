2eb79f5a194d5a9604b5372f0f9e65079317fe98
Amaresh <amaresh9973kumar@gmail.com>
2024-06-04 12:46:40 +0530

    Added Basic Folder Structuring

---
+++ ANDROID_CLIENT/app/src/main/java/com/cg/thirdeye/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.cg.thirdeye
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cg.thirdeye.ui.theme.ThirdEyeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ThirdEyeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Amaresh")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ThirdEyeTheme {
+        Greeting("Android")
+    }
+}
